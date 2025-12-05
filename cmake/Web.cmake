5c1f549277ad9c73d4d1ff2336f839590c540ac0
shadymeowy <tolgademirdal@gmail.com>
2025-12-05 22:35:07 +0100

    emscripten demo

--- cmake/Web.cmake
+++ cmake/Web.cmake
@@ -1,3 +1,31 @@
-# error out since not implemented yet
+# handle GL source files
+include(${CMAKE_CURRENT_LIST_DIR}/GL.cmake)
 
-message(FATAL_ERROR "Web build not implemented yet")
+# create the demo wasm target
+add_executable(glviskit_wasm
+    "src/main_wasm.cpp"
+    "${GLVISKIT_GL_SOURCES}"
+)
+
+# output html by default
+set_target_properties(glviskit_wasm PROPERTIES SUFFIX ".html")
+
+# include directories and compile/link options
+target_include_directories(glviskit_wasm PRIVATE
+    "${CMAKE_CURRENT_SOURCE_DIR}/include"
+)
+target_compile_options(glviskit_wasm PRIVATE
+    "-sUSE_SDL=3"
+)
+target_link_options(glviskit_wasm PRIVATE
+    "-sUSE_SDL=3"
+    "-sMIN_WEBGL_VERSION=2"
+    "-sMAX_WEBGL_VERSION=2"
+    "-sWASM=1"
+    "-sALLOW_MEMORY_GROWTH=1"
+)
+
+# link libraries
+target_link_libraries(glviskit_wasm PRIVATE
+    glm::glm
+)
---
+++ src/main_wasm.cpp
@@ -0,0 +1,144 @@
+#include <cmath>
+#include <random>
+
+#include "glviskit.hpp"
+
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL_main.h>
+
+namespace {
+std::shared_ptr<glviskit::RenderBuffer> render_buffer{nullptr};
+std::shared_ptr<glviskit::RenderBuffer> render_buffer_sine{nullptr};
+std::shared_ptr<glviskit::RenderBuffer> render_buffer_axes{nullptr};
+std::shared_ptr<glviskit::sdl::Window> window1{nullptr};
+// std::shared_ptr<glviskit::sdl::Window> window2{nullptr};
+}  // namespace
+
+auto SDL_AppInit(void ** /*appstate*/, int /*argc*/, char ** /*argv*/)
+    -> SDL_AppResult {
+    auto &manager = glviskit::Manager::GetInstance();
+
+    // create a window 1
+    window1 = manager.CreateWindow("Window1", 800, 600);
+
+    // create a window 2
+    // window2 = manager.CreateWindow("Window2", 800, 600);
+    render_buffer = manager.CreateRenderBuffer();
+    window1->AddRenderBuffer(render_buffer);
+    // window2->AddRenderBuffer(render_buffer);
+
+    render_buffer->ClearInstances();
+    for (int i = 1; i < 5; i++) {
+        const double s = (i % 2 == 0) ? 1 : -1;
+        render_buffer->AddInstance({3.0F * (i - 0.5), 0, 0}, {0.5F * s, 0, 0});
+        render_buffer->AddInstance({-3.0F * (i - 0.5), 0, 0},
+                                   {-0.5F * s, 0, 0});
+    }
+
+    render_buffer_sine = manager.CreateRenderBuffer();
+    window1->AddRenderBuffer(render_buffer_sine);
+    // window2->AddRenderBuffer(render_buffer_sine);
+
+    render_buffer_axes = manager.CreateRenderBuffer();
+    window1->AddRenderBuffer(render_buffer_axes);
+    // window2->AddRenderBuffer(render_buffer_axes);
+
+    render_buffer_axes->Size(5.0F);
+    render_buffer_axes->Color({1.0F, 0.0F, 0.0F, 1.0F});
+    render_buffer_axes->Line({0.0F, 0.0F, 0.0F}, {1.0F, 0.0F, 0.0F});
+    render_buffer_axes->Color({0.0F, 1.0F, 0.0F, 1.0F});
+    render_buffer_axes->Line({0.0F, 0.0F, 0.0F}, {0.0F, 1.0F, 0.0F});
+    render_buffer_axes->Color({0.0F, 0.0F, 1.0F, 1.0F});
+    render_buffer_axes->Line({0.0F, 0.0F, 0.0F}, {0.0F, 0.0F, 1.0F});
+
+    auto camera = window1->GetCamera();
+    camera->SetPerspectiveFov(60.0F, 60.0F);
+    camera->SetPosition({0.0F, 0.0F, 0.0F});
+    camera->SetRotation({0.0F, 0.0F, 0.0F});
+    camera->SetPreserveAspectRatio(true);
+    camera->SetDistance(15.0F);
+
+    // auto camera2 = window2->GetCamera();
+    // camera2->SetPerspectiveFov(60.0F, 60.0F);
+    // camera2->SetPosition({0.0F, 0.0F, 0.0F});
+    // camera2->SetRotation({0.0F, 0.0F, 0.0F});
+    // camera2->SetPreserveAspectRatio(true);
+    // camera2->SetDistance(15.0F);
+
+    return SDL_APP_CONTINUE;
+}
+
+auto SDL_AppIterate(void * /*appstate*/) -> SDL_AppResult {
+    const float curr_time = glviskit::Manager::GetTimeSeconds();
+
+    static float angle = 0.0F;
+    static int frame_index = 0;
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<float> dis(0.0F, 1.0F);
+    static auto rnf = [&]() -> float { return dis(gen); };
+
+    auto &manager = glviskit::Manager::GetInstance();
+    auto camera = window1->GetCamera();
+    // auto camera2 = window2->GetCamera();
+
+    frame_index++;
+    angle += 0.005F;
+    camera->SetRotation({-0.5F, angle, 0.0F});
+    // camera2->SetRotation({-0.5F, -angle, 0.0F});
+
+    for (int i = 0; i < 10; i++) {
+        render_buffer->Size((rnf() * 1.0F) + 1.0F);
+        render_buffer->Color({
+            rnf(),
+            rnf(),
+            rnf(),
+            rnf(),
+        });
+        render_buffer->Point({
+            (rnf() * 2.0F) - 1.0F,
+            (rnf() * 2.0F) - 1.0F,
+            (rnf() * 2.0F) - 1.0F,
+        });
+    }
+
+    if (frame_index % 10 == 0) {
+        render_buffer->Color({rnf(), rnf(), rnf(), rnf()});
+        render_buffer->Size(rnf() * 4.0F);
+        render_buffer->Line(
+            {
+                (rnf() * 2.0F) - 1.0F,
+                (rnf() * 2.0F) - 1.0F,
+                (rnf() * 2.0F) - 1.0F,
+            },
+            {
+                (rnf() * 2.0F) - 1.0F,
+                (rnf() * 2.0F) - 1.0F,
+                (rnf() * 2.0F) - 1.0F,
+            });
+    }
+
+    render_buffer_sine->Restore();
+    render_buffer_sine->Color({1.0F, 0.0F, 0.0F, 1.0F});
+    render_buffer_sine->Size(4.0F);
+    for (int ix = -1000; ix <= 1000; ix++) {
+        const float x = static_cast<float>(ix) / 1000.0F;
+        const float y = sinf((50.0F * x) + (10 * curr_time));
+        const float z = cosf((50.0F * x) + (10 * curr_time));
+
+        render_buffer_sine->Color(
+            {(x * 0.5F) + 0.5F, (y * 0.5F) + 0.5F, 0.5F, 1.0F});
+        render_buffer_sine->LineTo({20.0F * x, 1.5 * y, 1.5 * z});
+    }
+    render_buffer_sine->LineEnd();
+    manager.Render();
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void * /*appstate*/, SDL_AppResult /*result*/) {}
+
+auto SDL_AppEvent(void * /*appstate*/, SDL_Event *event) -> SDL_AppResult {
+    auto &manager = glviskit::Manager::GetInstance();
+    return manager.ProcessEvent(*event) ? SDL_APP_CONTINUE : SDL_APP_SUCCESS;
+}
